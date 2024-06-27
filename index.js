31ef7b926ea26916bf22e59a5a41f8081175423e
sundarrajcs <sundarrajcs1@gmail.com>
2024-06-27 17:03:49 +0530

    Updated Stopwatch

--- index.js
+++ index.js
@@ -4,33 +4,56 @@ var start = document.getElementById('start').addEventListener('click',startfun);
 var stop = document.getElementById('stop').addEventListener('click',stopfun);
 var resume = document.getElementById('resume').addEventListener('click',resumefun);
 
-    
+
+var millisecond = document.getElementById('millisecond');
+var sec = document.getElementById('sec');
+var min = document.getElementById('min');   
 
 var count = 0;
+var secondcount = 0;
+var mincount = 0;
 function starttime(){                    
     settimes = setInterval(()=>{        
         count += 1;
-        console.log(count) ;
-    },1000);
+        
+        millisecond.innerText = count.toString().padStart(3,0);
+        if(count == 100){            
+            count = 0;
+            secondcount += 1;            
+            sec.innerText = secondcount.toString().padStart(2,0);
+        }
+
+        if(secondcount == 10){
+            count = 0;
+            secondcount = 0; 
+            mincount += 1;           
+            min.innerText = mincount.toString().padStart(2,0);
+        }
+    },10);
 }
 
-function startfun(){
-    
+function startfun(){    
     starttime();
 }
 
 function resetfun(){
+    clearInterval(settimes);    
+    count = 0;
+    secondcount = 0;
+    mincount = 0;
+    millisecond.innerText = count.toString().padStart(3,0);
+    sec.innerText = secondcount.toString().padStart(2,0);
+    min.innerText = mincount.toString().padStart(2,0);
+    
     console.log('reset');
 }
 
-function stopfun(){ 
-    console.log('stop')               
+function stopfun(){     
     clearInterval(settimes);                     
 }
 
 function resumefun(){
-    starttime();
-    console.log('Resume');
+    starttime();    
 }
 
 
